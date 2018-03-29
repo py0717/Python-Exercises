b7cb18aa950c88f2b4636a5ffcf89b2522e19060
Pyung Yu <pyungyu@sas.upenn.edu>
2018-03-29 15:09:18 -0400

    Add files via upload

---
+++ reverse.string.py
@@ -0,0 +1,28 @@
+# Reverse String
+import unittest
+
+def reverse(phrase):
+    """ inputs string and returns reverse of that string """
+    return phrase[::-1]
+
+
+# test from exercism
+class ReverseStringTests(unittest.TestCase):
+    def test_empty_string(self):
+            self.assertEqual(reverse(''), '')
+
+    def test_a_word(self):
+            self.assertEqual(reverse('robot'), 'tobor')
+
+    def test_a_capitalized_word(self):
+            self.assertEqual(reverse('Ramen'), 'nemaR')
+
+    def test_a_sentence_with_punctuation(self):
+            self.assertEqual(reverse('I\'m hungry!'), '!yrgnuh m\'I')
+
+    def test_a_palindrome(self):
+            self.assertEqual(reverse('racecar'), 'racecar')
+
+
+# run test
+unittest.main()
